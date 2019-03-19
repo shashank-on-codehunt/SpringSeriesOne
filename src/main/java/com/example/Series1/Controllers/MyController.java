28bd9a4802d7e41c2fcb5ec5fc98838603d27c32
shashank-on-codehunt <shashank4387@gmail.com>
2019-03-19 19:52:21 +0530

    Calling through Application Context

---
+++ src/main/java/com/example/Series1/Controllers/MyController.java
@@ -0,0 +1,10 @@
+package com.example.Series1.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public static String showResult(){
+        return "Hello World";
+    }
+}
--- src/main/java/com/example/Series1/Series1Application.java
+++ src/main/java/com/example/Series1/Series1Application.java
@@ -1,11 +1,21 @@
 package com.example.Series1;
 
+import com.example.Series1.Controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Series1Application {
+	static ApplicationContext applicationContext;
 	public static void main(String[] args) {
-		SpringApplication.run(Series1Application.class, args);
+		applicationContext = SpringApplication.run(Series1Application.class, args);
+		new Series1Application().contextExperimentation();
+	}
+
+	private void contextExperimentation() {
+		//Make sure that the name of the bean is accordingly the first character should be interchanged with captial or small accordingly
+		MyController myController =  (MyController)applicationContext.getBean("myController");
+		System.out.println(myController.showResult());
 	}
 }
